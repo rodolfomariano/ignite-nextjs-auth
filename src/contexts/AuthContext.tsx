24a5793fdd0ccd4fd831870b4c38365e367b1222
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-05-11 07:45:50 -0300

    first commit

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,81 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import Router from 'next/router'
+import { parseCookies, setCookie } from 'nookies'
+import { api } from "../services/api";
+
+type User = {
+  email: string
+  permissions: string[]
+  roles: string[]
+}
+
+type SignInCredentials = {
+  email: string,
+  password: string
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>
+  user: User
+  isAuthenticated: boolean
+}
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>()
+  const isAuthenticated = !!user
+
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies()
+
+    if (token) {
+      api.get('/me').then(response => {
+        console.log(response)
+      })
+    }
+
+  }, [])
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      })
+
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      setUser({
+        email,
+        permissions,
+        roles
+      })
+
+      Router.push('/dashboard')
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
